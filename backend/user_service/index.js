3de0a8cb61d8abdd8d50dfd37a172a1fcc050bc1
Deepankar <dip2leo@gmail.com>
2023-03-19 15:11:23 -0400

    docker + endpoint test

--- backend/user_service/index.js
+++ backend/user_service/index.js
@@ -10,14 +10,20 @@ const app = express();
 const PORT = process.env.PORT || 4001;
 
 app.use(express.json());
-app.use(cors({
-    origin: [process.env.NODE_ENV === 'production' ? process.env.GATEWAY_HOST : "*"],
-}));
+app.use(
+  cors({
+    origin: [
+      process.env.NODE_ENV === "production" ? process.env.GATEWAY_HOST : "*",
+    ],
+  })
+);
 
 app.use("/", routes);
 
-initDatabase().then(() => {
+initDatabase()
+  .then(() => {
     app.listen(PORT, () => {
-        console.log(`User service is listening at PORT ${PORT}`);
+      console.log(`User service is listening at PORT ${PORT}`);
     });
-}).catch(err => console.error(err));
+  })
+  .catch((err) => console.error(err));
