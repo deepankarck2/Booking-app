818edf232a5b775995c8e4d7f73257b0eea13d7a
Mahfuzul Islam <mahfuzul.islam6543@gmail.com>
2023-05-01 20:42:09 -0400

    updated home.jsx

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,17 +1,27 @@
 import { useNavigate } from "react-router-dom"
+// import logo from "../Home/Images/airbnb-banner.jpeg"
+
 
 export default function Home() {
     const navigate = useNavigate();
 
     return (
-        <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
-            <h1 className="text-4xl font-bold text-gray-800 mb-6">Home</h1>
-            <button
-                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
-                onClick={() => navigate("/browse")}
-            >
-                Browse
-            </button>
+        <div style={{paddingTop: '10px'}}>
+            <div class="w-full bg-cover bg-center" style={{ height:'32rem',   backgroundImage: `url("https://media01.findrentals.com/rentals/9135/153059/banner-elk-home-boulder-sky-river-run-c5290294-full.jpeg")` }}>
+                <div class="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50">
+                    <div class="text-center">
+                        <h1 class="text-white text-2xl font-semibold uppercase md:text-3xl">Book your first house..</h1>
+                        <button
+                                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
+                                onClick={() => navigate("/browse")}
+                            >
+                                Browse
+                            </button>
+                    </div>
+                </div>
+            </div>
+            
         </div>
+
     );
 }
